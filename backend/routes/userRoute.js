e67e63b883413f6026199db90f60ee13f86e5c63
japhethh <boknoy32130@gmail.com>
2024-06-29 20:12:54 +0800

    remove the multer

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -4,19 +4,19 @@ import { loginUser, registerUser } from '../controllers/userController.js';
 import authMiddleware from '../middleware/Auth.js';
 
 const userRouter = express.Router();
-import multer from 'multer';
+// import multer from 'multer';
 
 
-const storage = multer.diskStorage({
-  destination:"./uploads",
-  filename:(req,file,cb)  => {
-    return cb(null,`${Date.now()}${file.originalname}`);
-  }
-});
+// const storage = multer.diskStorage({
+//   destination:"./uploads",
+//   filename:(req,file,cb)  => {
+//     return cb(null,`${Date.now()}${file.originalname}`);
+//   }
+// });
 
 const upload = multer({storage:storage});
 // ,upload.single("image")
-userRouter.post("/register",upload.single("image"),registerUser);
+userRouter.post("/register",registerUser);
 userRouter.post("/login",loginUser);
 
 
