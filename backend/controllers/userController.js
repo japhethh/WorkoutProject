fdcf507c5fd2eef8793ca43fa9c97a7dfe817d1d
japhethh <boknoy32130@gmail.com>
2024-06-30 04:12:18 +0800

    i'm still learning

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,5 @@
 import workoutModel from '../models/workoutModel.js'
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs';
 import jwt from "jsonwebtoken";
 import validator from 'validator';
 
