c0807fb7813ad3ee15e9257d57be1d5b9563f9d3
japhethh <boknoy32130@gmail.com>
2024-06-29 08:03:33 +0800

    +

--- backend/server.js
+++ backend/server.js
@@ -1,27 +1,27 @@
-import express from 'express';
-import cors from 'cors';
-import { ConnectDB } from './config/db.js';
-import workoutRouter from './routes/workoutRoute.js';
-import userRouter from './routes/userRoute.js';
-import "dotenv/config"
+import express from "express";
+import cors from "cors";
+import { ConnectDB } from "./config/db.js";
+import workoutRouter from "./routes/workoutRoute.js";
+import userRouter from "./routes/userRoute.js";
+import "dotenv/config";
 
 const app = express();
 const port = 4000;
 
-// app.use(cors( 
+// app.use(cors(
 //     {
 //       origin:["https://deploy-mern-1whq.vercel.app"],
 //       methods:["POST","GET"],
 //       credentials:true,
 //     }
 // ));
-app.use(cors( 
-    {
-      origin:["https://workout-project-api.vercel.app"],
-      methods:["POST","GET"],
-      credentials:true,
-    }
-));
+app.use(
+  cors({
+    origin: "https://workout-project-bpsz416wu-japhethhs-projects.vercel.app",
+    methods: ["POST", "GET", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 // app.use(cors())
 app.use(express.json());
 
@@ -29,12 +29,12 @@ ConnectDB();
 
 app.get("/", (req, res) => {
   // res.send("<h2 className='text-orange-500'> Api working!  </h2>")
-  res.json("Hello")
-})
+  res.json("Hello");
+});
 
-app.use("/api/workout",workoutRouter)
-app.use("/api/user",userRouter);
+app.use("/api/workout", workoutRouter);
+app.use("/api/user", userRouter);
 
 app.listen(port, () => {
-  console.log(` Server Started on http://localhost:${port}`)
-})
+  console.log(` Server Started on http://localhost:${port}`);
+});
