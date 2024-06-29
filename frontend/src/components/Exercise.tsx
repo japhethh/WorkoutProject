90afc5a54767ce558f15518a2285605228165a80
japhethh <boknoy32130@gmail.com>
2024-06-29 08:54:48 +0800

    Removing the Error in Exercise

--- frontend/src/components/Exercise.tsx
+++ frontend/src/components/Exercise.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from "react";
+import  { useContext, useEffect, useState } from "react";
 import { PiTrashSimpleFill } from "react-icons/pi";
 import { WorkoutContext } from '../context/WorkoutContext';
 
