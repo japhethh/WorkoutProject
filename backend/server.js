1c816d44356f3cb63b569b00d19f41b207172500
japhethh <boknoy32130@gmail.com>
2024-06-29 21:05:25 +0800

    I think this is the last one but i try pa

--- backend/server.js
+++ backend/server.js
@@ -11,20 +11,27 @@ const port = process.env.PORT || 3000;
 app.use(
   cors({
     origin: "https://workout-project-frontend.vercel.app",
-    methods: ["POST", "GET","PUT","DELETE"],
+    methods: ["POST", "GET", "PUT", "DELETE"],
     credentials: true,
   })
 );
-
-// app.use(cors());
 
 app.use(express.json());
 
 ConnectDB();
 
-  app.get("/", (req, res) => {
-    res.send("Hello, worlds!"); // Sending a plain text response
-  });
+app.get("/", (req, res) => {
+  res.send("Hello, worlds!"); // Sending a plain text response
+});
+
+// Set CORS headers for all routes
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "https://workout-project-frontend.vercel.app");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  next();
+});
 
 app.use("/api/workout", workoutRouter);
 app.use("/api/user", userRouter);
