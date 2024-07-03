b847b19e112b6ee678fa3b4a3bf186f4e9be699b
japhethh <boknoy32130@gmail.com>
2024-07-03 13:21:32 +0800

    Render Successfully Debug

--- frontend/src/components/AddExercise.tsx
+++ frontend/src/components/AddExercise.tsx
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { useContext, useState, useEffect } from 'react'
+import { useContext, useState } from 'react'
 import { toast } from 'react-toastify';
 import { WorkoutContext } from '../context/WorkoutContext.tsx';
 
