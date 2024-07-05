70b7edd306f71849028bdaedd9bc43bb8869c519
japhethh <boknoy32130@gmail.com>
2024-07-06 06:01:06 +0800

    Fix unused

--- admin/src/components/Sidebar.tsx
+++ admin/src/components/Sidebar.tsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useState } from "react";
+// import { createContext, useContext, useState } from "react";
 import { HiOutlineDotsVertical } from "react-icons/hi";
 
 import { NavLink } from "react-router-dom";
