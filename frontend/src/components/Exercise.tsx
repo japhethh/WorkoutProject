9cdbbb7b624fc417732fadfaa64b249da2558853
japhethh <boknoy32130@gmail.com>
2024-07-12 13:54:31 +0800

    Update again

--- frontend/src/components/Exercise.tsx
+++ frontend/src/components/Exercise.tsx
@@ -61,7 +61,7 @@ const Exercise = ({ darkMode }: Dark) => {
   const filterExercises = filter === "ALL" ? data.exercises : data.exercises.filter((item) => item.focusArea === filter);
 
   return (
-    <div className="max-md:overflow-y-auto overflow-y-scroll lg:h-[420px]">
+    <div >
 
       <div>
         <ul className="flex gap-2 text-lg text-paragraph font-semibold cursor-pointer max-md:overflow-x-scroll max-md:w-6/6 max-md:mx-auto max-md:border-t-2  max-md:border-b-2 max-md:border-gray-500 py-3 px-1  ">
@@ -69,72 +69,73 @@ const Exercise = ({ darkMode }: Dark) => {
             onClick={() => handleFilterClick("ARM")}>ARM</li>
           <li className={`px-2 py-1  rounded-md ${filter === "BACK" ? "bg-blue-200" : "bg-gray-200"}`}
             onClick={() => handleFilterClick("BACK")}>BACK</li>
-          <li className={`px-2 py-1  rounded-md ${filter === "CHECK" ? "bg-blue-200" : "bg-gray-200"}`}
-            onClick={() => handleFilterClick("CHECK")}>CHECK</li>
+          <li className={`px-2 py-1  rounded-md ${filter === "CHEST" ? "bg-blue-200" : "bg-gray-200"}`}
+            onClick={() => handleFilterClick("CHEST")}>CHEST</li>
           <li className={`px-2 py-1  rounded-md ${filter === "SHOULDER" ? "bg-blue-200" : "bg-gray-200"}`}
             onClick={() => handleFilterClick("SHOULDER")}>SHOULDER</li>
           <li className={`px-2 py-1  rounded-md ${filter === "LEGS" ? "bg-blue-200" : "bg-gray-200"}`}
             onClick={() => handleFilterClick("LEGS")}>LEGS</li>
         </ul>
       </div>
+      <div className="max-md:overflow-y-auto overflow-y-scroll lg:h-[420px]">
+        <div>
+          {filterExercises.length === 0 ? (
+            <>
+              <div className="text-center text-paragraph flex justify-center items-center h-64 w-full">
+                No exercises available
+              </div>
+            </>
+          )
+            :
+            filterExercises.map((item) => (
+              <div key={item._id} className="shadow-md rounded-md p-4 mb-2">
+                <div className="flex justify-between items-center">
+                  <h1 className="text-md text-green-600 font-semibold">
+                    {item.name}
+                  </h1>
+                  <button
+                    type="button"
+                    className="bg-gray-200 rounded-md p-3 "
+                    onClick={() => openModal(item._id)}
+                  >
+                    <PiTrashSimpleFill className="text-paragraph" />
+                  </button>
+                </div>
+                <div className="text-paragraph">
+                  <h1>Focus Area: <span className="font-semibold">{item.focusArea}</span></h1>
+                  <h1>Sets: <span className="font-semibold">{item.set}</span></h1>
+                  <h1>Reps:  <span className="font-semibold">{item.rep}</span></h1>
+                </div>
+              </div>
+            ))
+          }
 
-      <div>
-        {filterExercises.length === 0 ? (
-          <>
-            <div className="text-center text-paragraph flex justify-center items-center h-64 w-full">
-              No exercises available
-            </div>
-          </>
-        )
-          :
-          filterExercises.map((item) => (
-            <div key={item._id} className="shadow-md rounded-md p-4 mb-2">
-              <div className="flex justify-between items-center">
-                <h1 className="text-md text-green-600 font-semibold">
-                  {item.name}
-                </h1>
+
+        </div>
+
+        {selectedItemId && (
+          <dialog id="my_modal_5" className="modal modal-bottom sm:modal-middle" open={showModal}>
+            <div className="modal-box bg-background border">
+              <h3 className="font-bold text-lg text-paragraph">Confirm Deletion</h3>
+              <p className="py-4 text-paragraph">Are you sure you want to delete this exercise?</p>
+              <div className="modal-action">
                 <button
-                  type="button"
-                  className="bg-gray-200 rounded-md p-3 "
-                  onClick={() => openModal(item._id)}
+                  className={`justify-center items-center flex gap-2 py-3 px-5 font-semibold rounded-xl ${darkMode === "light" ? "bg-white text-[#1D232A]" : "bg-black text-white"}`}
+                  onClick={() => handleDeleteClick(selectedItemId)}
                 >
-                  <PiTrashSimpleFill className="text-paragraph" />
+                  Delete
+                </button>
+                <button
+                  className={`justify-center items-center flex gap-2 py-3 px-5 font-semibold rounded-xl ${darkMode === "light" ? "bg-white text-[#1D232A]" : "bg-black text-white"}`}
+                  onClick={closeModal}
+                >
+                  Close
                 </button>
               </div>
-              <div className="text-paragraph">
-                <h1>Focus Area: <span className="font-semibold">{item.focusArea}</span></h1>
-                <h1>Sets: <span className="font-semibold">{item.set}</span></h1>
-                <h1>Reps:  <span className="font-semibold">{item.rep}</span></h1>
-              </div>
             </div>
-          ))
-        }
-
-
+          </dialog>
+        )}
       </div>
-
-      {selectedItemId && (
-        <dialog id="my_modal_5" className="modal modal-bottom sm:modal-middle" open={showModal}>
-          <div className="modal-box bg-background border">
-            <h3 className="font-bold text-lg text-paragraph">Confirm Deletion</h3>
-            <p className="py-4 text-paragraph">Are you sure you want to delete this exercise?</p>
-            <div className="modal-action">
-              <button
-                className={`justify-center items-center flex gap-2 py-3 px-5 font-semibold rounded-xl ${darkMode === "light" ? "bg-white text-[#1D232A]" : "bg-black text-white"}`}
-                onClick={() => handleDeleteClick(selectedItemId)}
-              >
-                Delete
-              </button>
-              <button
-                className={`justify-center items-center flex gap-2 py-3 px-5 font-semibold rounded-xl ${darkMode === "light" ? "bg-white text-[#1D232A]" : "bg-black text-white"}`}
-                onClick={closeModal}
-              >
-                Close
-              </button>
-            </div>
-          </div>
-        </dialog>
-      )}
     </div>
   );
 };
