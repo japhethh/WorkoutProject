0a6b36865cb3747fa8828b41fe7a58b171e62534
japhethh <boknoy32130@gmail.com>
2025-01-07 15:53:16 +0800

    Remove unused imports and commented-out code in UserQueries context

--- frontend/src/context/UserQueries.tsx
+++ frontend/src/context/UserQueries.tsx
@@ -1,26 +1,26 @@
-import axios from "axios"
-import { apiURL } from "./Store"
-import { WorkoutContext } from "./WorkoutContext"
-import { useContext } from "react"
-import create from 'zustand '
+// import axios from "axios"
+// import { apiURL } from "./Store"
+// import { WorkoutContext } from "./WorkoutContext"
+// import { useContext } from "react"
+// import create from 'zustand'
 
 
 
 
-const exampleZustand = create((set) => ({
-  user: null,
-  setUser:
-}))
+// const exampleZustand = create((set) => ({
+//   user: null,
+//   setUser:
+// }))
 
 
 const totalUsers = async () => {
-  const context = useContext(WorkoutContext)
-  if (!context) {
-    return null;
-  }
-  const response = await axios.get(`${apiURL}/api/user/totalUsers`, {
-    headers: { Authorization: `Bearer ${token}` }
-  })
+  // const context = useContext(WorkoutContext)
+  // if (!context) {
+  //   return null;
+  // }
+  // const response = await axios.get(`${apiURL}/api/user/totalUsers`, {
+  //   headers: { Authorization: `Bearer ${token}` }
+  // })
 }
 
 export { totalUsers }
